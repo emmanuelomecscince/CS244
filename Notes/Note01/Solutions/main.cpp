f016cc3058c516590aba7556c8776898b5584955
emmanuelomecscince <Emmanuel.osimokun@mec.science>
2025-01-30 12:43:29 -0500

    Update main.cpp

--- Notes/Note01/Solutions/main.cpp
+++ Notes/Note01/Solutions/main.cpp
@@ -6,6 +6,14 @@
  * You must use control structures and functions
 */
 
+void display(string board)
+{
+     for(int i = 0; i < 9;1 += 1)
+{ 
+     stdit:: cout board[i];
+
+     if((i+1) % 3 == 0)
+{    
 int main()
 {
 	return 0;
